cf238b26787bfb3f2648c30f49c0168b3f6f0d78
Alex <darkprince3738@gmail.com>
2021-05-30 14:10:43 +0300

    One Task

---
+++ Two Classes/Task1/Math.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    static class Math
+    {
+        public const double PI = 3.14;
+        public const double E = 1.67;
+        public static int MyFavoriteNumber = 7;
+
+        public static int Max(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        public static int Min(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            return b;
+
+        }
+
+        public static int Min(int[] a)
+        {
+            int k = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (k > a[i])
+                {
+                    k = a[i];
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ Two Classes/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pi = Math.PI;
+            var e = Math.E;
+
+            Console.WriteLine($"PI = {pi}");
+            Console.WriteLine($"E = {e}");
+            Console.WriteLine($"MyFavoriteNumber = {Math.MyFavoriteNumber}");
+
+            Console.WriteLine("Write two numbers");
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Max = {Math.Max(a, b)}");
+            Console.WriteLine($"Min = {Math.Min(a, b)}");
+            Console.WriteLine($"Min = {Math.Min(new int[] { a, b, Math.MyFavoriteNumber })}");
+        }
+    }
+}
